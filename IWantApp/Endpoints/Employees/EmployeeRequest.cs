8558d01b7cc045cf70448b64b364b6618fb73093
Yago Araujo <yagoarjbarbosa@gmail.com>
2022-08-11 21:04:52 -0300

    first employee endpoint

---
+++ IWantApp/Endpoints/Employees/EmployeeRequest.cs
@@ -0,0 +1,3 @@
+﻿namespace IWantApp.Endpoints.Employees;
+
+public record EmployeeRequest(string Name, string Email, string Password);
--- IWantApp/Program.cs
+++ IWantApp/Program.cs
@@ -1,4 +1,5 @@
 using IWantApp.Endpoints.Categories;
+using IWantApp.Endpoints.Employees;
 using IWantApp.Infra.Data;
 using Microsoft.AspNetCore.Identity;
 
@@ -26,5 +27,7 @@ app.UseHttpsRedirection();
 app.MapMethods(CategoryPost.Template, CategoryPost.Methods, CategoryPost.Handle);
 app.MapMethods(CategoryGetAll.Template, CategoryGetAll.Methods, CategoryGetAll.Handle);
 app.MapMethods(CategoryPut.Template, CategoryPut.Methods, CategoryPut.Handle);
+
+app.MapMethods(EmployeePost.Template, EmployeePost.Methods, EmployeePost.Handle);
 
 app.Run();
